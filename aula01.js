a50b95c5e03d969af178a3342c0f8b0862f69f00
FRANCISCO RODRIGO VIEIRA NOGUEIRA <francisconogueira8987@digitalcollege.internal>
2025-04-29 20:09:39 -0300

    Inclusão do tipos de dados JS e Aula 01 JS

---
+++ aula01.js
@@ -0,0 +1,9 @@
+// criação de variável (const, let, var)
+const n1 = 10;
+const n2 = 40;
+
+//soma
+const resultado = n1 + n2 // 50
+
+//exibir o resultado da soma
+//console.log(resultado) // 50
---
+++ tipos_dados.js
@@ -0,0 +1,22 @@
+//var, const e let
+
+//1-const (constante)
+const pi = 3.14;
+
+let nome = 'Rodrigo'
+console.log(nome) // Rodrigo
+
+nome = 'Rodrigo Nogueira'
+console.log(nome) // Rodrigo Nogueira
+
+let desconto = 20.5
+//multiplicando a variável sem alterar o valor original
+console.log(2*desconto) 
+
+desconto = desconto * 2
+console.log('Desconto atualizado: ' + desconto)
+
+//Array (lista)
+let tamanhos = ['P', 'M', 'G', 'GG']
+//console.log(tamanhos) // P,M,G,GG         
+const numero_1
