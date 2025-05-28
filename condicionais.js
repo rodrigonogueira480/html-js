31215a58c366fc863ec2e9dd3a1d22665cb8f6ff
FRANCISCO RODRIGO VIEIRA NOGUEIRA <francisconogueira8987@digitalcollege.internal>
2025-05-27 21:50:19 -0300

    Adicionado Função e condicional

--- condicionais.js
+++ condicionais.js
@@ -1,10 +1,20 @@
-const idade = 60;
-console.log(`Idade: ${idade} anos`); 
+const idade = 50;
+console.log(`Idade: ${idade} anos`);
 
 
 //if, else
-if (idade >= 60) {
+/*if (idade >= 60) {
     console.log('Você é idoso');
 } else {
     console.log('Você não é idoso');
-} 
+} */
+
+if (idade >= 60) {
+    console.log(`Véi`);
+} else if (idade >= 12 && idade <= 18) {
+    console.log(`Você é aborrecente`);
+} else if (idade < 12) {
+    console.log(`Tira a catinga do mijo`);
+} else {
+    console.log(`Você é adulto`);
+}
--- funcao.js
+++ funcao.js
@@ -1,5 +1,5 @@
 //Sintaxe da função
-function nome(/*argumentos, parâmetros*/) {//são entradas de dados para a função utilizar
+/*function nome(/*argumentos, parâmetros*//*) {//são entradas de dados para a função utilizar
     //bloco de código/ lógica
 }
 
@@ -21,5 +21,26 @@ function somarDoisNumeros(n1, n2) {
 
 //Os parâmetros/argumentos 
 somarDoisNumeros(20, 50) //undefined, pois não tem parâmetros na função
-somarDoisNumeros(100, 400) //500
+somarDoisNumeros(100, 400) //500*/
 
+function verificarIdade(idade) {
+    if (typeof idade !== 'string') {
+        const mensagem = `Idade: ${idade} anos`
+        console.log(mensagem);
+
+
+if (idade >= 60) {
+    console.log(`Véi`);
+} else if (idade >= 12 && idade <= 18) {
+    console.log(`Você é aborrecente`);
+} else if (idade < 12) {
+    console.log(`Tira a catinga do mijo`);
+} else {
+    console.log(`Você é adulto`);
+}
+} else {
+        console.log('Por favor, insira uma idade válida.');
+    }       
+}
+
+verificarIdade('60') // Chamada da função com o parâmetro idade
